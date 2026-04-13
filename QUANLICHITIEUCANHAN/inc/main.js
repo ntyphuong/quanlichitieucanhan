040221fb0cdce8afd376986acd19c91ccf067d68
ntyphuong <phuong_dth235740@student.agu.edu.vn>
2026-04-13 14:26:40 +0700

    Upload code

--- QUANLICHITIEUCANHAN/inc/main.js
+++ QUANLICHITIEUCANHAN/inc/main.js
@@ -1,4 +1,3 @@
-// inc/main.js
 import { auth, provider } from './config.js';
 import { signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
@@ -7,7 +6,7 @@ const btnLogout = document.getElementById('btn-logout');
 const authSection = document.getElementById('auth-section');
 const userInfo = document.getElementById('user-info');
 
-// Xử lý nút Đăng nhập tại chỗ 
+// Xử lý nút Đăng nhập  
 if(btnLogin) {
     btnLogin.onclick = async () => {
         try {
@@ -17,7 +16,7 @@ if(btnLogin) {
         }
     };
 }
-// Xử lý nút Đăng xuất tại chỗ 
+// Xử lý nút Đăng xuất 
 if(btnLogout) {
     btnLogout.onclick = async () => {
         if(confirm("Bạn muốn đăng xuất khỏi hệ thống?")) {
@@ -29,7 +28,6 @@ if(btnLogout) {
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
-        // Có user -> Hiện thông tin, ẩn nút Login
         authSection.classList.add('d-none');
         userInfo.classList.remove('d-none');
         
@@ -37,10 +35,8 @@ onAuthStateChanged(auth, (user) => {
         document.getElementById('user-email').innerText = user.email;
         document.getElementById('user-avatar').src = user.photoURL;
         
-        // Lưu UID vào bộ nhớ trình duyệt để các trang khác dùng dễ hơn
         localStorage.setItem("user_uid", user.uid);
     } else {
-        // Không có user -> Hiện nút Login, ẩn thông tin
         authSection.classList.remove('d-none');
         userInfo.classList.add('d-none');
         localStorage.removeItem("user_uid");
